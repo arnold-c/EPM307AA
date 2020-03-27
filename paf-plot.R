7c438f014572d549bd07109d7783fed104e155a8
Callum Arnold <lonca9@student.london.ac.uk>
2020-03-27 01:18:46 -0400

    create paf plot with multivariate ORs

--- paf-plot.R
+++ paf-plot.R
@@ -6,6 +6,9 @@ library(ggpubr)
 paf <- read_excel(here::here("data", "risk-factors.xlsx"), 
                          sheet = "PAF")
 
+aor <- read_excel(here::here("data", "risk-factors.xlsx"), 
+                  sheet = "aOR")
+
 paf %<>%
   mutate(Exposure = case_when(
     Exposure == "Not breast feeding at any stage of life" ~
@@ -20,6 +23,21 @@ order <- c("Not breast feeding \nat any stage of life", "Prone sleeping \npositi
            "Smoking during \npregnancy")
 
 paf$Exposure <- factor(paf$Exposure, levels = order)
+
+aor %<>%
+  mutate(Exposure = case_when(
+    Exposure == "Not breast feeding at any stage of life" ~
+      "Not breast feeding \nat any stage of life",
+    Exposure == "Prone sleeping position" ~ "Prone sleeping \nposition *",
+    Exposure == "Smoking during pregnancy" ~ "Smoking during \npregnancy",
+    Exposure == "Not sharing parental bedroom" ~ "Not sharing \nparental bedroom",
+    TRUE ~ Exposure))
+
+order <- c("Not breast feeding \nat any stage of life", "Prone sleeping \nposition *",
+           "Not sharing \nparental bedroom", "Bed sharing",
+           "Smoking during \npregnancy")
+
+aor$Exposure <- factor(aor$Exposure, levels = order)
 
 
 # PAF Bar Plot ------------------------------------------------------------
@@ -68,6 +86,9 @@ or_table <- table_df %>%
   replace(is.na(.), "-") %>%
   arrange(desc(Exposure))
 
+
+# PAF and OR Plot ---------------------------------------------------------
+
 paf_table_p <- ggtexttable(paf_table, rows = NULL, 
             theme = ttheme("default"))
 
@@ -85,3 +106,53 @@ paf_comb_p <- ggarrange(paf_plot,
 
 ggsave(filename = "paf-plot.png", plot = paf_comb_p, path = here::here("out"),
        width = 23, height = 15, units = "cm")
+
+
+# aOR Table ---------------------------------------------------------------
+
+aor_summary <- aor %>%
+  mutate_if(is.numeric, round, digits = 2) %>%
+  mutate(aOR = case_when(
+    is.na(Caveat) == TRUE ~ paste0(
+    sprintf("%.2f", aOR), 
+    " (", 
+    sprintf("%.2f",`Lower CI`), 
+    ", ", 
+    sprintf("%.2f",`Upper CI`), 
+    ")"),
+    TRUE ~ paste0(
+      sprintf("%.2f", aOR), 
+      " (", 
+      sprintf("%.2f",`Lower CI`), 
+      ", ", 
+      sprintf("%.2f",`Upper CI`), 
+      ")", 
+      "\n ",
+      Caveat)
+  ))
+
+aor_table <- aor_summary %>%
+  select(-c(Caveat, `Lower CI`, `Upper CI`)) %>%
+  pivot_wider(names_from = Study, values_from = "aOR") %>%
+  replace(is.na(.), "-") %>%
+  arrange(desc(Exposure))
+
+
+
+# PAF and aOR Plot --------------------------------------------------------
+
+aor_table_p <- ggtexttable(aor_table, rows = NULL, 
+                          theme = ttheme("default"))
+
+aor_comb_p <- ggarrange(paf_plot, 
+                        ggarrange(aor_table_p, or_table_p, 
+                                  nrow = 2,
+                                  labels = c("Odds Ratio (Multivariate)",
+                                             "Odds Ratio (Univariate)"),
+                                  hjust = c(-0.17, -0.6),
+                                  vjust = c(1, 1)),
+                        ncol = 2)
+
+ggsave(filename = "paf-aor-plot.png", plot = aor_comb_p, path = here::here("out"),
+       width = 23, height = 15, units = "cm")
+
