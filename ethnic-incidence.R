b148b9fe0f77460b3af7454ae03d97e00ab1383c
Callum Arnold <lonca9@student.london.ac.uk>
2020-03-26 21:50:53 -0400

    Create plot to show incidence by ethnic group

---
+++ ethnic-incidence.R
@@ -0,0 +1,66 @@
+library(tidyverse)
+library(readxl)
+library(magrittr)
+library(ggpubr)
+library(ggsci)
+
+df <- read_excel(here::here("data", "incidence.xlsx"), sheet = "Ethnic Group")
+
+df %<>%
+  mutate(Type = case_when(
+    Type == "SIDS" ~ "SIDS deaths",
+    Type == "SUDI" ~ "SUDI deaths",
+    TRUE ~ Type)
+  )
+         
+df %<>%
+  pivot_longer(cols = c(`2006`:`2015`), 
+               names_to = "Year", values_to = "Number") %>%
+  pivot_wider(names_from = "Type", values_from = "Number") %>%
+  mutate(`SIDS rate` = `SIDS deaths` * 1000 / `Live Births`,
+         `SUDI rate` = `SUDI deaths` * 1000 / `Live Births`
+         ) %>%
+  mutate_if(is.numeric, round, digits = 2)
+
+# Incidence Plot ----------------------------------------------------------
+
+SIDS_inc <- ggplot(data = df, 
+       mapping = aes(x = Year, y = `SIDS rate`, 
+                     color = `Ethnic Group`)) +
+  geom_line(aes(group = `Ethnic Group`), 
+            size = 2) +
+  geom_point(size = 4) +
+  scale_color_manual(values = alpha(c("#0073C2FF", "#EFC000FF", "#CD534CFF",
+                                      "#868686FF", "black"), 
+                                    c(0.3, 0.3, 0.3, 0.3, 1))) +
+  ylab("Rate (per 1000 live births)") +
+  theme_pubr() +
+  theme(panel.grid.major.y = element_line(color = "grey80",
+                                          linetype = "longdash"),
+        axis.title.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.x = element_blank())
+
+SUDI_inc <- ggplot(data = df, 
+                   mapping = aes(x = Year, y = `SUDI rate`, 
+                                 color = `Ethnic Group`)) +
+  geom_line(aes(group = `Ethnic Group`), 
+            size = 2) +
+  geom_point(size = 4) +
+  scale_color_manual(values = alpha(c("#0073C2FF", "#EFC000FF", "#CD534CFF",
+                                      "#868686FF", "black"), 
+                                    c(0.3, 0.3, 0.3, 0.3, 1))) +
+  ylab("Rate (per 1000 live births)") +
+  theme_pubr() +
+  theme(panel.grid.major.y = element_line(color = "grey80",
+                                          linetype = "longdash"))
+
+comb_plot <- ggarrange(SIDS_inc, SUDI_inc,
+          ncol = 1,
+          align = "h",
+          common.legend = TRUE,
+          labels = c("A", "B"))
+
+ggsave(filename = "inc.png", plot = comb_plot, path = here::here("out"),
+       width = 16, height = 18, units = "cm")  
+
